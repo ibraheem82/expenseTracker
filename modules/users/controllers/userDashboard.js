489da01ad603285b14178a8a09d51de433c1e4bc
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-04-20 22:52:24 +0100

    user dashboard was done, and greeting for user that is visiting dashboard was implemented...

---
+++ modules/users/controllers/userDashboard.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const userDashboard = async (req, res) => {
+    const usersModel = mongoose.model("users");
+    console.log(req.user._id)
+
+    const getUser = await usersModel.findOne({
+        _id : req.user._id, // getting the user by the request that have been attached to them. still getting the user by thier id, to get thier actual data.
+        
+    }).select("-password -name");// what you are excluding.
+    // OR 
+    // .select("name balance email"); // what we want to display in the dashboard.
+    // Get current hour
+    const currentHour = new Date().getHours();
+
+    let greeting;
+    if (currentHour >= 5 && currentHour < 12) {
+        greeting = "Good morning";
+    } else if (currentHour >= 12 && currentHour < 17) {
+        greeting = "Good afternoon";
+    } else {
+        greeting = "Good evening";
+    }
+
+
+    res.status(200).json({
+        status: "success",
+        message: `${greeting}  ${getUser.name} 👋`,
+        data: getUser,
+    });
+};
+
+module.exports = userDashboard;

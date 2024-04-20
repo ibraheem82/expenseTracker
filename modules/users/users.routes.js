96eaf88498cea9886dbacdc8007457506d4ff151
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-04-20 22:13:47 +0100

    auth middleware was added ...

--- modules/users/users.routes.js
+++ modules/users/users.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const register = require("./controllers/register");
 const login = require("./controllers/login");
 const userDashboard = require("./controllers/userDashboard");
+const auth = require("../../middleware/auth");
 
 const userRoutes = express.Router();
 
@@ -9,6 +10,7 @@ const userRoutes = express.Router();
 
 userRoutes.post("/register", register)
 userRoutes.post("/login", login)
+userRoutes.use(auth); // auth middleware is run first before dashboard...
 userRoutes.get("/dashboard", userDashboard)
 
 module.exports = userRoutes;

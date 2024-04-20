6ba5d2fbd81dce43003e3c7b3188a346961fb6eb
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-04-20 18:19:52 +0100

    validations for fields added ...

--- modules/users/controllers/register.js
+++ modules/users/controllers/register.js
@@ -5,6 +5,14 @@ const register = async (req, res) => {
 
     const {email, password, confirm_password, name, balance} = req.body;
 
+    // ! Validations 
+    //  && email === ""
+    if(!email) throw "Email must be provided!";
+    if(!password) throw "Password must be provided";
+    if(password.length < 5) throw "Password must be at least 5 characters long."
+    if(!name) throw "Name is required";
+    if(password !== confirm_password) throw "Password and confirmed password must match."
+
     const getDuplicateEmail = await usersModel.findOne({
         email:email
     });

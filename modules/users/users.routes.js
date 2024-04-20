91f00a44a3824f419f81a43e24ec6f493f995ba6
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-04-20 19:35:21 +0100

    dashboard route was added  ...

--- modules/users/users.routes.js
+++ modules/users/users.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const register = require("./controllers/register");
 const login = require("./controllers/login");
+const userDashboard = require("./controllers/userDashboard");
 
 const userRoutes = express.Router();
 
@@ -8,5 +9,6 @@ const userRoutes = express.Router();
 
 userRoutes.post("/register", register)
 userRoutes.post("/login", login)
+userRoutes.get("/dashboard", userDashboard)
 
 module.exports = userRoutes;

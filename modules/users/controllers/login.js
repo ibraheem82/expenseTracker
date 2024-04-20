b958b57980255ca3644f8516693dca3171347f28
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-04-20 20:29:34 +0100

    jwt was use to to encrypt the user data...

--- modules/users/controllers/login.js
+++ modules/users/controllers/login.js
@@ -21,9 +21,18 @@ const getUser = await usersModel.findOne({
 
     if(!comparePassword) throw "Email and password do not match.";
 
+    const accessToken = await jsonwebtoken.sign({ // sign function to create a JSON Web Token (JWT).
+        //This is a JavaScript object that contains the data you want to embed within the JWT. Here
+        _id: getUser._id,
+        name:getUser.name
+    }, 
+    "this-is-a-secret-key"
+)
+
     res.status(200).json({
         status: "success",
-        message: "User logged in successfully"
+        message: "User logged in successfully",
+        accessToken: accessToken // the accessToken contains the informations of the user.
     });
 }
 

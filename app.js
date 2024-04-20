2ed158facdd31a68d1046c259138a14bb29e67a0
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-04-20 15:10:41 +0100

    Handler middleware added ..

---
+++ app.js
@@ -0,0 +1,24 @@
+require('express-async-errors');
+
+const express = require("express");
+const errorHandler = require("./handlers/errorHandler");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+
+mongoose.connect(process.env.mongo_connection, {}).then(() => {
+            console.log("Mongo connection successful!");
+        }).catch(() => {
+            console.log("Mongo connection failed");
+        });
+
+app.use(express.json());
+app.use(errorHandler);
+
+app.listen(8000, () => {
+    console.log("Server started ...")
+})
+
+

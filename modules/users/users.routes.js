01a6e320d5f78b9220c1d5622a78fb4e05b5f2d4
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-04-20 19:26:58 +0100

    route for login added ...

--- modules/users/users.routes.js
+++ modules/users/users.routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const register = require("./controllers/register");
+const login = require("./controllers/login");
 
 const userRoutes = express.Router();
 
 // ** Routes
 
 userRoutes.post("/register", register)
+userRoutes.post("/login", login)
 
 module.exports = userRoutes;

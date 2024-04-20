54ec2b17898912a7a0e50f539d50925c9e918667
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-04-20 17:54:31 +0100

    code was refactored to throw the actual error message in the pipeline.

--- handlers/errorHandler.js
+++ handlers/errorHandler.js
@@ -1,9 +1,18 @@
 const errorHandler = (error, req, res, next) => {
     if(error){
-        res.status(400).json({
-            status:"failed",
-            error: error
-        })
+
+        if(error.message){
+            res.status(400).json({
+                status:"failed",
+                error: error.message
+            });
+        } else{
+            res.status(400).json({
+                status:"failed",
+                error: error
+            });
+        }
+        
     } else{
         next();
     }

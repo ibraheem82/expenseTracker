0a8cc897ab4456b172d95fe7693efa4dd44c26c8
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-04-21 10:39:54 +0100

    route to get all transactions was added ...

--- modules/transactions/transactions.routes.js
+++ modules/transactions/transactions.routes.js
@@ -3,6 +3,7 @@ const express = require("express");
 const auth = require("../../middleware/auth");
 const addIncome = require("./controllers/addIncome");
 const addExpense = require("./controllers/addExpense");
+const getTransactions = require("./controllers/getTransactions");
 
 const transactionRoutes = express.Router();
 
@@ -15,5 +16,6 @@ transactionRoutes.use(auth); // auth middleware is run first before dashboard...
 
 transactionRoutes.post('/addIncome', addIncome);
 transactionRoutes.post('/addExpense', addExpense);
+transactionRoutes.get('/', getTransactions);
 
 module.exports = transactionRoutes;

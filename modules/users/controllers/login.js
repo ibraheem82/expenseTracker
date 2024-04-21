0f8b7862db58f5a95f96c951f929aa2f18c40af2
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-04-21 07:03:40 +0100

    code was refactored to use a centralized jwt manager...

--- modules/users/controllers/login.js
+++ modules/users/controllers/login.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-const jsonwebtoken = require("jsonwebtoken");
+const jwtManager = require("../../../managers/jwtManager");
+
 
 const login = async (req, res) => {
 
@@ -19,13 +20,9 @@ const getUser = await usersModel.findOne({
 
     if(!comparePassword) throw "Email and password do not match.";
 
-    const accessToken = await jsonwebtoken.sign({ // sign function to create a JSON Web Token (JWT).
-        //This is a JavaScript object that contains the data you want to embed within the JWT. Here
-        _id: getUser._id,
-        name:getUser.name
-    }, 
-   process.env.jwt_salt
-)
+    const accessToken = jwtManager(getUser);
+
+    
 
     res.status(200).json({
         status: "success",

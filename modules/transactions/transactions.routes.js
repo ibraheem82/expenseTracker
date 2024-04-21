7fd853b5359aaef201938a844b838e92bbb54c7f
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-04-21 10:13:58 +0100

    addExpense route added, and chained to the addExpense controller...

--- modules/transactions/transactions.routes.js
+++ modules/transactions/transactions.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const auth = require("../../middleware/auth");
 const addIncome = require("./controllers/addIncome");
+const addExpense = require("./controllers/addExpense");
 
 const transactionRoutes = express.Router();
 
@@ -13,5 +14,6 @@ transactionRoutes.use(auth); // auth middleware is run first before dashboard...
 // ** Protected routes
 
 transactionRoutes.post('/addIncome', addIncome);
+transactionRoutes.post('/addExpense', addExpense);
 
 module.exports = transactionRoutes;

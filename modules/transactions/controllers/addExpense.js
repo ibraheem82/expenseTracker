8249a8bf85b5f7f678046c139b44d6c966d7695a
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-04-21 10:13:30 +0100

    addExpense logic done, when the user hit the expense route thier amount the deducted .....

---
+++ modules/transactions/controllers/addExpense.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const addExpense = async (req, res) => {
+    const usersModel = mongoose.model("users");
+    const transactionsModel  = mongoose.model("transactions");
+
+    const {amount, remarks} = req.body;
+
+    if(!amount) throw "Amount is required";
+    if(!remarks) throw "Remarks is required";
+    if(remarks.length < 5) throw "Remarks must be at least 5 characters long!";
+
+    if(!validator.isNumeric(amount.toString())) throw "Amount must be a valid number.";
+
+    if(amount < 0) throw "Amount must not be negative";
+
+    await transactionsModel.create({
+        user_id: req.user._id, // assigning created transaction to this user.
+        amount: amount,
+        remarks: remarks,
+        transaction_type: "expense",
+    });
+
+    await usersModel.updateOne({ 
+        _id: req.user._id,
+    },{
+    $inc: { // it will decrease.
+        balance:amount * -1
+    },
+},
+{
+    runValidators:true
+}
+
+)
+
+    res.status(200).json({
+        status: "Success!",
+        message: "Expense added successfully!",
+    });
+    
+}
+
+module.exports = addExpense

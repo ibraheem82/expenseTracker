2feaac2c4513061e15b45fa314721aff678d9505
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-04-22 15:48:10 +0100

    reset password route done..

--- modules/users/users.routes.js
+++ modules/users/users.routes.js
@@ -4,6 +4,7 @@ const login = require("./controllers/login");
 const userDashboard = require("./controllers/userDashboard");
 const auth = require("../../middleware/auth");
 const forgotPassword = require("./controllers/forgotPassword");
+const resetPassword = require("./controllers/resetPassword");
 
 const userRoutes = express.Router();
 
@@ -12,6 +13,7 @@ const userRoutes = express.Router();
 userRoutes.post("/register", register)
 userRoutes.post("/login", login)
 userRoutes.post("/forgotpw", forgotPassword)
+userRoutes.post("/resetpw", resetPassword)
 userRoutes.use(auth); // auth middleware is run first before dashboard...
 userRoutes.get("/dashboard", userDashboard)
 

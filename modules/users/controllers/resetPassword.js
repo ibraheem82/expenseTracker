e169d2cd6fd112e04e389c3cb2f6d403d495eaf8
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-04-23 15:42:16 +0100

    implements a secure password reset functionality. It retrieves user data, validates it, verifies a reset code, securely hashes the new password, and updates the user's password in the database.

---
+++ modules/users/controllers/resetPassword.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+
+const resetPassword = async (req, res) => {
+
+    const usersModel = mongoose.model("users");
+
+    const {email, new_password, reset_code} = req.body;
+
+    if(!email) throw "Email is required";
+    if(!new_password) throw "Please provide new password!";
+    if(!reset_code) throw "Reset code is required.";
+    if(new_password.length < 5) throw "Password must be at least 5 characters long."
+
+    // It uses the usersModel to find a user with the provided email and reset_code. This verifies if the email and reset code combination is valid.
+// 
+    const getUserWithResetCode = await usersModel.findOne({
+        email:email,
+        reset_code: reset_code,
+    })
+
+
+    if(!getUserWithResetCode) throw "Reset code does not match!";
+
+    const hashedPassword = await bcrypt.hash(new_password, 15)
+
+    // Finally, it uses usersModel.updateOne to update the password for the user with the matching email. It updates the password field with the hashed password obtained earlier.
+// The runValidators: true option ensures any validation rules defined on the user model are applied before saving the update.
+    await usersModel.updateOne({
+        email: email,
+    },
+    {  
+        password: hashedPassword
+    },
+
+    {
+        runValidators: true
+    }
+)
+
+
+    res.status(200).json({
+        status:"Success",
+        message: " Password resetted successfully"
+    })
+}
+module.exports = resetPassword;

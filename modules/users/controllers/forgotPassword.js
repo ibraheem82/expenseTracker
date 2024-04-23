9dd42391260d21a4970707513e7ee214c3d5d6d2
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-04-23 18:02:11 +0100

    emailManager Imported.... , ||  feat: Implement password reset email with verification code
    - Added functionality to send an email with a 5-digit verification code for password reset.
    - Improved security by requiring a verification code before allowing password reset.
    - The email body includes a clear message, personalized greeting, and the verification code.

--- modules/users/controllers/forgotPassword.js
+++ modules/users/controllers/forgotPassword.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const nodemailer = require('nodemailer');
+const emailManager = require("../../../managers/emailManager");
 const forgotPassword = async (req, res) => {
 
     const usersModel = mongoose.model("users");
@@ -26,51 +26,36 @@ const forgotPassword = async (req, res) => {
 );
 
 
-let transporter = nodemailer.createTransport({
-    host: process.env.mail_host,
-    port: process.env.mail_port,
-    auth: {
-      user: process.env.mail_email,
-      pass: process.env.mail_pass
-    }
-  });
 
-  const mailOptions = {
-    to: email,
-    from: "astro@buld.com",
-    subject: "Reset Your password.",
-    html: `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Reset Password</title>
-      </head>
-      <body style="font-family: Arial, sans-serif;">
-  
-        <table cellpadding="0" cellspacing="0" border="0" width="100%">
-          <tr>
-            <td style="background-color: #f7f7f7; padding: 20px; text-align: center;">
-              <h1 style="color: #333333;">Reset Your password 🔑</h1>
-              <b> Hi ${getUser.name}</b>
-              <p style="color: #666666; font-size: 16px;">Your 5-Digit verification code is ${resetCode}.</p>
-            </td>
-          </tr>
-        </table>
-  
-      </body>
-      </html>
-    `
-  };
-  
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error('Error occurred while sending email:', error);
-    } else {
-      console.log('Email sent:', info.response);
-    }
-  });
+
+
+
+  await emailManager(email,  "Reset Your password.",
+  `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reset Password</title>
+  </head>
+  <body style="font-family: Arial, sans-serif;">
+
+    <table cellpadding="0" cellspacing="0" border="0" width="100%">
+      <tr>
+        <td style="background-color: #f7f7f7; padding: 20px; text-align: center;">
+          <h1 style="color: #333333;">Reset Your password 🔑</h1>
+          <b> Hi ${getUser.name}</b>
+          <p style="color: #666666; font-size: 16px;">Your 5-Digit verification code is ${resetCode}.</p>
+        </td>
+      </tr>
+    </table>
+
+  </body>
+  </html>
+`
+    )
+
 
     res.status(200).json({
         status : `Reset Code sent to ${email} successfully.`

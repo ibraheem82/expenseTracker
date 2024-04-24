344d4e03a40ee45348aa7c389d6efaacf060ac07
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-04-24 17:11:15 +0100

    delete transaction route deleted...

--- modules/transactions/transactions.routes.js
+++ modules/transactions/transactions.routes.js
@@ -4,6 +4,7 @@ const auth = require("../../middleware/auth");
 const addIncome = require("./controllers/addIncome");
 const addExpense = require("./controllers/addExpense");
 const getTransactions = require("./controllers/getTransactions");
+const deleteTransaction = require("./controllers/deleteTransaction");
 
 const transactionRoutes = express.Router();
 
@@ -17,5 +18,6 @@ transactionRoutes.use(auth); // auth middleware is run first before dashboard...
 transactionRoutes.post('/addIncome', addIncome);
 transactionRoutes.post('/addExpense', addExpense);
 transactionRoutes.get('/', getTransactions);
+transactionRoutes.delete('/:transaction_id', deleteTransaction);
 
 module.exports = transactionRoutes;
